6604fa0eff5624a5ded242b911a0ea4d3f7af4f3
Joshua C. Randall <jcrandall@alum.mit.edu>
2021-03-04 02:05:53 +0000

    resolve missing_docs linter warnings

--- src/vtab/mod.rs
+++ src/vtab/mod.rs
@@ -377,9 +377,27 @@ impl IndexInfo {
     }
 
     // TODO idxFlags
-    // TODO colUsed
 
-    // TODO sqlite3_vtab_collation (http://sqlite.org/c3ref/vtab_collation.html)
+    /// Information about the columns required for the query
+    #[cfg(feature = "vtab_col_used")] // SQLite >= 3.10.0
+    pub fn col_used(&self) -> u64 {
+        unsafe { (*self.0).colUsed as u64 }
+    }
+
+    /// Determine the collation for a virtual table constraint
+    /// https://sqlite.org/c3ref/vtab_collation.html
+    /// May only be called during the best_index call
+    #[cfg(feature = "vtab_collation")] // SQLite >= 3.22.0
+    pub fn collation(&self, constraint_idx: usize) -> Result<&str> {
+        use std::ffi::CStr;
+        let constraint_idx_c = constraint_idx as c_int;
+        let collation_c_buf = unsafe { ffi::sqlite3_vtab_collation(self.0, constraint_idx_c) };
+        let collation_c_str = unsafe { CStr::from_ptr(collation_c_buf) };
+        match collation_c_str.to_str() {
+            Ok(collation) => Ok(collation),
+            Err(err) => Err(Error::Utf8Error(err)),
+        }
+    }
 }
 
 /// `feature = "vtab"`
